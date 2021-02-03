3d2f10aa9d8c91a8eea18973fe84c68dd828278f
Dario Pozzi <dariopozzi3@gmail.com>
2021-02-03 12:21:00 -0500

    Settato DB e connesso a server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
 const mongoose = require("mongoose");
 const serverName = "server";
-const port = 5000;
+const port = 7000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -15,5 +15,27 @@ mongoose.connect("mongodb://localhost:27017/" + serverName, {
 });
 
 const connection = mongoose.connection;
+
+const daySchema = new mongoose.Schema({
+  numero: { type: Number },
+  riordino: { type: Boolean },
+  yoga: { type: Boolean },
+  coding: { type: Boolean },
+});
+const Day = mongoose.model("Day", daySchema);
+
+const day = new Day({
+  numero: 1,
+  riordino: 0,
+  yoga: 0,
+  coding: 1,
+});
+
+app.get("/api/calendar", function (req, res) {
+  Day.find({}, function (err, foundDays) {
+    res.json(foundDays);
+  });
+  console.log("Successfully zero");
+});
 
 app.listen(port, () => console.log("Server started on port " + port));
